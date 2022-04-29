afac5bb4d989e84954d04c7020a4cbb4a9f053ec
Ale <alegonzalez1993@gmail.com>
2022-04-28 22:28:23 -0400

    Finished project

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,48 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container">
+      <h1 class="text-center mt-4 text-secondary">Crea una nueva tarea</h1>
+      <div class="input-group mt-3">
+        <input v-model="tarea" type="text" class="form-control" placeholder="Ingresa nueva tarea">
+        <button @click="addTarea" class="btn btn-outline-secondary" type="button">Crear</button>
+      </div>
+      <h2 class="text-center my-5 text-secondary">Lista</h2>
+      <ol>
+        <div v-for="(tarea, i) in tareas" :key="i" class="lista">
+          <li class="mb-3">{{ tarea }}</li> 
+          <button @click="tareas.splice(index, 1)" type="button" class="btn btn-outline-danger btn-sm mb-3">Eliminar</button>
+        </div>
+        
+      </ol>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data : () => ({
+    tarea : '',
+    tareas : []
+  }),
+  methods: {
+    addTarea() {
+      const { tarea } = this
+      this.tareas.push(tarea)
+      this.tarea = ''
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.lista {
+  display: flex;
+  justify-content: center;
+}
+
+.lista > li {
+  width: 50%;
 }
 </style>
